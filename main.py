dff4c5fefe09e1a4aa758505c0c05d686a0765c8
eloza <lozaedward@gmail.com>
2024-05-08 14:51:58 -0700

    Update models.py to include list of users

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import User, Gender, Role
 
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Janet",
+        last_name="Weiss",
+        gender=Gender.female,
+        roles=[Role.student],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Brad",
+        last_name="Majors",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user],
+    ),
+]
 
 
 @app.get("/")
