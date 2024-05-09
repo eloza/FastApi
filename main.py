63752bceef72c67f805ac695681d8a28b68cb1d2
eloza <lozaedward@gmail.com>
2024-05-09 14:03:15 -0700

    Add delete endpoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List
-from uuid import uuid4
+from uuid import UUID, uuid4
 from fastapi import FastAPI
 from models import User, Gender, Role
 
@@ -38,3 +38,11 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
