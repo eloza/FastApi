e78f9c9482d8ed9b9b0851348e13129c26c424d1
eloza <lozaedward@gmail.com>
2024-06-12 13:17:03 -0700

    Add user Registration test

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 from httpx import AsyncClient
-from uuid import UUID
+from uuid import UUID, uuid4
 from fastapi import FastAPI
 from main import app  # Adjust the import according to your project structure
 
@@ -22,3 +22,17 @@ async def test_fetch_users(client):
     response = await client.get("/api/v1/users")
     assert response.status_code == 200
     assert len(response.json()) == 2  # based on the initial db size
+
+# Testing User Registration
+@pytest.mark.asyncio
+async def test_register_user(client):
+    new_user = {
+        "id": str(uuid4()),
+        "first_name": "Riff",
+        "last_name": "Raff",
+        "gender": "male",
+        "roles": ["admin"]
+    }
+    response = await client.post("/api/v1/users", json=new_user)
+    assert response.status_code == 200
+    assert UUID(response.json()["id"]) == UUID(new_user["id"])
